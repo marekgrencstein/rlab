85389333469c6899c2c663373146411dd7d78561
marekgrencstein <marekgrencstein@gmail.com>
2015-08-27 12:53:55 +0300

    Update booking.js

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -7,6 +7,7 @@ $( window ).resize( function() {
 
 $(document).ready(function()
 {
+
 	LAB_SETTINGS();
 	CALCULATE_VARIABLES();
 	BOOKINGS();
@@ -20,12 +21,12 @@ function calculateMaxSteps()
 {
 	taken_width = parseInt($("#navigator").outerWidth()) + start_offset;
 	free_width = parseInt($( window ).innerWidth()) - taken_width;
-	max_steps = parseInt((free_width - 100) / time_step);
+	max_steps = parseInt( ((free_width - 100) / time_step )*(60 / stretch));
 }
 // GLOBAL VARIABLES
 
 max_steps = 30; // how many steps we fit onto screen at a time
-stretch = 60; // how many px we stretch 1 hour time;
+stretch = 30; // how many px we stretch 1 hour time;
 
 // LAB SETTINGS
 max_times = 5; // how many times can user book at a time
@@ -33,7 +34,11 @@ time_offset = 0;
 time_step = 15; // each unit = 15 minutes
 
 // CALCULATED VARIABLES
-book_start_time = 0;
+var book_start_time = 0;
+var book_start_year = 0;
+var book_start_month = 0;
+var book_start_day   = 0;
+var timezone_offset  = 0;
 
 // BOOKINGS
 var book_times_start = [];
@@ -48,25 +53,19 @@ function LAB_SETTINGS()
 
 function BOOKINGS()
 {
-	/*
-	book_times_start = [ book_start_time + 3, book_start_time + 10, book_start_time + 17 ];
-	book_times_steps = [ 2, 5 , 10];
-	//*/
-
-	
-
 	for( i=0; i < jasonbookings.length; i++ )
 	{
 		
 		var book = JSON.parse(jasonbookings[ i ]);
 		
-		
 		var start_date = new Date( book.start );
-		startTime = parseInt( (start_date.getMinutes() + (start_date.getHours())*60) / time_step );
-
 		
+		var dayOffset  = start_date.getDate() - book_start_day;
+		
+		startTime = parseInt( (dayOffset*60*24 + start_date.getMinutes() + (start_date.getHours())*60 + timezone_offset) / time_step );
+
 		book_times_start.push( startTime );
-		book_times_steps.push( parseInt(book.step) );
+		book_times_steps.push( parseInt(book.step) + 1);
 		
 	}
 	
@@ -77,6 +76,11 @@ function CALCULATE_VARIABLES()
 {
 	var date = new Date();
 	minutes = date.getMinutes() + date.getHours()*60;
+	
+	timezone_offset  = date.getTimezoneOffset();
+	book_start_year  = 1900 + date.getYear();
+	book_start_month = 1 + date.getMonth();
+	book_start_day   = date.getDate();
 
 	roundedMinutes = minutes - ( (minutes + 60 - time_offset) % time_step );
 	book_start_time = roundedMinutes / time_step;
@@ -256,7 +260,7 @@ function clickBook( index, element )
 			possible_book_end += 1;
 		}
 	}
-	else if( index < selected_book_index || index > selected_book_index + possible_book_end )
+	else if( index < selected_book_index || index > selected_book_index + possible_book_end || locked )
 	{
 		locked = false;
 		clearSelectedBook();
@@ -325,12 +329,87 @@ function generateBookingForm()
 	html+="Book from "+ minutesToTime( (startTime)*time_step ) +" to "+minutesToTime( (startTime + selected_book_steps + 1)*time_step );
 	html+=" as <select id=\"isGroup\"><option value=\"false\">User</option><option value=\"true\">Group</option>" + "</select>";
 	
-	html+="<input type=\"button\" value=\"Book\" id=\"book-button\" onclick=\"sendBookingData();\"/>";
+	html+="<input type=\"button\" value=\"Book\" class=\"button\" onclick=\"sendBookingData();\"/>";
 	html+="<p><i>Click outside of possible book times to clear</i></p>"
 	$("#book-info").html( html );
 }
 
 function sendBookingData()
 {
-	window.location="?start="+(book_start_time + selected_book_index)+"&step="+selected_book_steps+"&isGroup="+$("#isGroup").val();
+	var year  = book_start_year;
+	var month = book_start_month;
+	var day   = book_start_day;
+	
+	var startTime = (book_start_time + timeline_offset + selected_book_index ) * time_step;
+	
+	var hour  = parseInt(startTime / 60) % 24;
+	var minute= parseInt(startTime % 60);
+
+	// fix incorrect day and month
+	day += parseInt(startTime / 1440);
+	
+	if( (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31 )
+	{
+		day -= 31;
+		month++;
+		if( month > 12 )
+		{
+			month = 0;
+			year++;
+		}
+	}
+	else if( month != 2 && day > 30)
+	{
+		day -= 30;
+		month++;
+		if( month > 12 )
+		{
+			month = 0;
+			year++;
+		}
+	}
+	else if( day > 28 )
+	{
+		day -= 28;
+		month++;
+	}
+
+	var query = [
+		["booking[start(1i)]" , year],
+		["booking[start(2i)]", month],
+		["booking[start(3i)]", day],
+		["booking[start(4i)]", hour],
+		["booking[start(5i)]", minute],
+		["booking[step]", selected_book_steps ],
+		["booking[lab_id]",1],
+		["booking[user_id]",1],
+		["booking[group_id]",1],
+		["commit","Create Booking"]
+	];
+
+	post("/bookings", query, 'post' ); 
+}
+
+function post(path, params, method) 
+{
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+	
+	for(var i = 0; i < params.length; i++)
+	{
+		var hiddenField = document.createElement("input");
+		hiddenField.setAttribute("type", "hidden");
+		hiddenField.setAttribute("name",  params[i][0]);
+		hiddenField.setAttribute("value", params[i][1]);
+		
+		form.appendChild(hiddenField);
+	}
+
+    document.body.appendChild(form);
+    form.submit();
 }
