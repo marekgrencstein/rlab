31b56c24816a11b9560b58e872e71bd50a39ac63
marekgrencstein <marekgrencstein@gmail.com>
2015-08-27 12:56:27 +0300

    Update lab_settings_helper.rb

--- app/helpers/lab_settings_helper.rb
+++ app/helpers/lab_settings_helper.rb
@@ -1,8 +1,8 @@
 module LabSettingsHelper
-	def jasonlabsettings(lab_id)
+	def jasonlabsettings
 		
 		
-			@labsettings=LabSetting.where("lab_id = ?",lab_id)
+			@labsettings=LabSetting.where("lab_id = ?",session[:lab_id])
 			raw(@labsettings.first.to_json only: [:max_steps,:total_steps,:step_length])
 	end
 end
